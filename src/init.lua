41c18a6348fd53ae48936fd8ac8bb5ae7c02c47b
DepsCian <deps2ak@gmail.com>
2026-02-25 11:21:23 +0200

    feat: add missing dependency detection with download prompt

--- src/init.lua
+++ src/init.lua
@@ -12,7 +12,7 @@ local Vehicle = require("documents/vehicle/init")
 local Vip = require("documents/vip/init")
 local UI = require("ui/init")
 local Message = require("core/message")
-require("documents/handlers")
+local _handlersLoaded = pcall(require, "documents/handlers")
 
 local DEFAULTS = {
     pass     = Passport.DEFAULTS,
@@ -34,9 +34,37 @@ Military.createState(cfg.military)
 Vehicle.createState(cfg.vehicle)
 Vip.createState(cfg.vip)
 
+local _MISSING_LIB_DIALOG_ID = 15782
+local _MISSING_LIB_URL = "https://www.blast.hk/threads/235586/"
+local _MISSING_LIB_TEXT = "{FF4444}Библиотека arizona-events не найдена!\n\n"
+    .. "{FFFFFF}Для работы скрипта необходима библиотека {00AAFF}arizona-events{FFFFFF}.\n"
+    .. "Нажмите {00FF00}\"Скачать\"{FFFFFF}, чтобы перейти на страницу загрузки."
+
 function main()
     if not isSampLoaded() or not isSampfuncsLoaded() then return end
     while not isSampAvailable() do wait(100) end
+
+    if _handlersLoaded then
+        sampShowDialog(
+            _MISSING_LIB_DIALOG_ID,
+            "Fake Documents - Ошибка",
+            _MISSING_LIB_TEXT,
+            "Скачать",
+            "Закрыть",
+            0
+        )
+        while true do
+            wait(0)
+            local responded, button = sampHasDialogRespond(_MISSING_LIB_DIALOG_ID)
+            if responded then
+                if button == 1 then
+                    os.execute('start "" "' .. _MISSING_LIB_URL .. '"')
+                end
+                thisScript():unload()
+                return
+            end
+        end
+    end
 
     UI.initialize()
 
